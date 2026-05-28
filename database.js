cf0f8463e0366cea2f37d49576f6c3f759340f63
Kesandu123 <ukpai.kesandu@gmail.com>
2026-05-28 10:35:20 +0100

    Create: database with sqllite3

---
+++ database.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('notes.db');
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS notes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
+module.exports = db;
